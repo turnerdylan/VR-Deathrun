0db5f336a905ae654d2871fc3920103929da934a
Dylan Turner <dytu4626@colorado.edu>
2019-11-21 10:28:22 -0700

    added stuff

---
+++ VR_Deathrun/Assets/ThisIsAThing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThisIsAThing : MonoBehaviour
+{
+    float xMove;
+    float zMove;
+    Rigidbody rb;
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMove = Input.GetAxis("Horizontal");
+        zMove = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(xMove, 0, zMove) * moveSpeed;
+    }
+}
