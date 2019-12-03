c6f5f2980af5900809ab33935c87c3140a8926b1
Willie <willie.kirby14@gmail.com>
2019-12-03 10:18:14 -0700

    Added "Activator"

---
+++ VR_Deathrun/Assets/Scripts/ActivatorSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatorSpawner : MonoBehaviour
+{
+    public GameObject spawnedObject;
+    private Vector3 pos;
+    public float xPos;
+    public float yPos;
+    public float zPos;
+    public bool activated;
+    public float cooldown;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activated = false;
+        pos = new Vector3(xPos, yPos, zPos);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(activated){
+            Instantiate(spawnedObject, pos, Quaternion.identity);
+            activated = false;
+        }
+    }
+}
---
+++ VR_Deathrun/Assets/Scripts/raycastToTarget.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycastToTarget : MonoBehaviour
+{
+    private int targetLayer = 1 << 8; // Layer 8 (targets)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            // The Unity raycast hit object, which will store the output of our raycast
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            // Parameters: position to start the ray, direction to project the ray, output for raycast, limit of ray length, and layer mask
+            if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, targetLayer)) {
+                // The object we hit will be in the collider property of our hit object.
+                // We can get the name of that object by accessing it's Game Object then the name property
+                Debug.Log(hit.collider.gameObject.name);
+                //activation code
+                hit.collider.gameObject.GetComponent<ActivatorSpawner>().activated = true;
+               
+               
+            }
+        }
+    }
+}
