871a45b16a74b0e0bcf74b1d715f8760fba80d8a
Willie <willie.kirby14@gmail.com>
2019-12-03 10:45:45 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ VR_Deathrun/Assets/Scripts/Rotater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    
+    public bool activated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activated){
+            this.transform.Rotate(10, 0, 0);
+        }
+    }
+}
